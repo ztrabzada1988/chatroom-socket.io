efc130b877768245afc646d79c0b5a2f037fa2f7
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-01-27 17:09:34 -0800

    comunication between client and server

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    // creates Manager object that allows automatic attempt to connect to server which allow you to send and receive messages
+    var socket = io();
     var input = $('input');
     var messages = $('#messages');
 
@@ -13,6 +15,10 @@ $(document).ready(function () {
 
         var message = input.val();
         addMessage(message);
+        // sends message to Socket.IO server. 1st Arg: what to call the sending item so we just call it 'message', 2nd Arg: The second argument is some data to attach to our message. In this case it's the contents of the text box.
+        socket.emit('message', message);
         input.val('');
     });
+    // listen for incoming messages/replies from server
+    socket.on('message', addMessage);
 });
--- server.js
+++ server.js
@@ -1,5 +1,24 @@
+var socket_io = require('socket.io');
+var http = require('http');
 var express = require('express');
 
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+// wrap the express app in http server so Socket.io runs alongside express
+var server = http.Server(app);
+var io = socket_io(server);
+
+io.on('connection', function (socket) {
+    console.log('Client connected');
+
+    socket.on('message', function (message) {
+        console.log('Received message:', message);
+        socket.broadcast.emit('message', message);
+    });
+});
+
+server.listen(process.env.PORT || 8080);
+
+// app.listen(process.env.PORT || 8080);
+console.log("server listening on local host 8080");
