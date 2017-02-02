2e1e9b9aaa473fbbd755a552f7a19c4873df461f
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-02-01 18:17:05 -0800

    updating chatroom server

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var countUsers = 0;
 io.on('connection', function (socket) {
     countUsers++
     console.log(countUsers + ' Client(s) connected');
+    socket.emit("userId", socket.id);
     // send (emit) countUsers to with name: numberOfClients to be captured in main.js
     io.emit('numberOfClients', countUsers);
 
@@ -24,6 +25,5 @@ io.on('connection', function (socket) {
 });
 
 server.listen(process.env.PORT || 8080);
-
 // app.listen(process.env.PORT || 8080);
 console.log("server listening on local host 8080");
